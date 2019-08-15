7b8ed750b96aa5d835b6fb56c83fd28ce9f1728b
bigfish0214 <lvhao999@163.com>
2019-08-15 23:49:34 +0800

    224

---
+++ imooc-ad-service/ad-binlog-kafka/src/main/java/com/imooc/ad/BinlogKafkaApplication.java
@@ -0,0 +1,16 @@
+package com.imooc.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * <h1>Binlog Kafka 启动程序</h1>
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class BinlogKafkaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BinlogKafkaApplication.class, args);
+    }
+}
